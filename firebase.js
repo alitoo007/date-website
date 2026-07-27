56ab5137eda96bab764f7afbcb5a00a96b58ac3a
alitoo007 <alitobonjour@gmail.com>
2026-07-27 04:16:52 +0100

    Refactor Firebase initialization with placeholders
    Updated Firebase configuration with placeholders for API keys and project details.

--- firebase.js
+++ firebase.js
@@ -1,51 +1,28 @@
-
-import { db } from "./firebase.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
 
 import {
-    ref,
-    push
+    getDatabase
 } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-database.js";
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA1ChUuQ37VrAGPtbKhGSAUWd5j8smltWU",
-  authDomain: "date-website-3972c.firebaseapp.com",
-  projectId: "date-website-3972c",
-  storageBucket: "date-website-3972c.firebasestorage.app",
-  messagingSenderId: "962194139737",
-  appId: "1:962194139737:web:aaa2fe6b269ed33e799f48",
-  measurementId: "G-B2DM0Y55VY"
+
+    apiKey: "YOUR_API_KEY",
+
+    authDomain: "YOUR_PROJECT.firebaseapp.com",
+
+    databaseURL:
+    "https://YOUR_PROJECT-default-rtdb.europe-west1.firebasedatabase.app",
+
+    projectId: "YOUR_PROJECT",
+
+    storageBucket: "YOUR_PROJECT.firebasestorage.app",
+
+    messagingSenderId: "YOUR_SENDER_ID",
+
+    appId: "YOUR_APP_ID"
+
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-async function saveResponse(date, time, food) {
 
-    try {
-
-        await push(ref(db, "responses"), {
-            date: date,
-            time: time,
-            food: food,
-            submittedAt: new Date().toISOString()
-        });
-
-        alert("Your response has been saved ❤️");
-
-    } catch (error) {
-        console.error(error);
-        alert("Something went wrong.");
-    }
-}
-const date = document.getElementById("date").value;
-const time = document.getElementById("time").value;
-const food = document.getElementById("food").value;
-
-saveResponse(date, time, food);
+export const db = getDatabase(app);
